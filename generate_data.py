26b0ef198d8811eaf03b3accfa0ad008030709fa
Chen He <chenhe@Chens-MacBook-Pro.local>
2020-09-21 18:29:21 -0700

    added working likelihood base code and script for making inverse covariance.

---
+++ generate_data.py
@@ -0,0 +1,18 @@
+from spherelikes.model import ModelCalculator
+
+
+def main():
+    args = {
+        'model_name': 'sim_data',
+        'model_yaml_file': './inputs/sample_fid_model.yaml',
+        'cobaya_yaml_file': './inputs/sample.yaml',
+        'output_dir': './data/sim_data/',
+    }
+
+    calc = ModelCalculator(args)
+    results = calc.get_and_save_results()
+    calc.check_load_results()
+
+
+if __name__ == '__main__':
+    main()
