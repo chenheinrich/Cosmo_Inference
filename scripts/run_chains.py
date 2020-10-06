262b17296eff4712f92ea83a53d80fc4120ea741
Chen Heinrich <chenhein@login4.stampede2.tacc.utexas.edu>
2020-10-06 18:52:05 -0500

    edited scripts/run_chains.py

--- scripts/run_chains.py
+++ scripts/run_chains.py
@@ -1,10 +1,18 @@
+import sys
+import argparse
+
 from cobaya.run import run
 from cobaya.yaml import yaml_load_file
-import sys
 
 if __name__ == '__main__':
 
-    info = yaml_load_file("./inputs/cobaya_pars/ps_base.yaml")
+    """
+    Example usage:
+        python3 scripts/run_chains.py ./inputs/simulated_chains_pars/sim.yaml -d -f
+    """
+
+    args = yaml_load_file(sys.argv[1])
+    info = yaml_load_file(args['cobaya_yaml_file'])
 
     for k, v in {"-f": "force", "-r": "resume", "-d": "debug"}.items():
         if k in sys.argv:
