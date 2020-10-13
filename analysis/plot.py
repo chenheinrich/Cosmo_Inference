ae192c4c5d87e4c5587f6464396a4c5c88acd899
Chen Heinrich <chenhe@caltech.edu>
2020-10-12 17:51:44 -0700

    added ability to give prior ranges to getdist automatically for making posterior plots

--- analysis/plot.py
+++ analysis/plot.py
@@ -2,8 +2,10 @@ import numpy as numpy
 import matplotlib.pyplot as plt 
 import os
 import sys
+import copy
 
-from getdist import plots
+from getdist import plots as gplots
+import getdist
 from cobaya.yaml import yaml_load_file
 
 #TODO need to make this flexible for different classes
@@ -35,7 +37,8 @@ class ChainPlotter():
 
         self.params_base = ['fnl', 'logA']
         # TODO might want to put this somewhere central
-        self.params_lcdm = ['fnl', 'logA', 'ns', 'nrun', 'theta_MC_100', 'ombh2', 'omch2', 'omegak', 'tau'] 
+        self.params_lcdm = ['fnl', 'logA', 'ns', 'nrun', 'theta_MC_100', \
+            'ombh2', 'omch2', 'omegak', 'mnu', 'w', 'wa', 'tau'] 
         self.params_bias = self.get_params_bias(range(self.nsample), range(self.nz))
         self.params_sys = []
         self.params = self.params_lcdm + self.params_bias + self.params_sys
@@ -48,7 +51,6 @@ class ChainPlotter():
         self.sim_values = dict(self.bias_default_values, **self.lcdm_sim_values) #TODO add sys params in the future
 
         self.priors = self.get_priors()
-
 
     def get_params_bias(self, isamples, izs):
         names = []
@@ -81,7 +83,8 @@ class ChainPlotter():
         for isample in range(self.nsample):
             params = self.get_params_bias([isample], range(self.nz))
             params.extend(self.params_base)
-            plot_name = os.path.join(self.plot_dir, 'plot_%s_bias_sample_%s.png'%(plot_type, isample+1))
+            plot_name = os.path.join(self.plot_dir, \
+                'plot_%s_bias_sample_%s.png'%(plot_type, isample+1))
             self.plot_params(plot_name, params=params, plot_type=plot_type)
 
     def plot_params(self, plot_name, params=None, plot_type='1d'):
@@ -89,41 +92,55 @@ class ChainPlotter():
 
         params = (params or self.params)
 
-        g = plots.get_subplot_plotter(
+        g = gplots.get_subplot_plotter(
             chain_dir=self.chain_dir, 
             analysis_settings=self.analysis_settings)
+
+        mcsamples = self.get_mcsamples(g, params)
+
         plot_func = {'1d': 'plots_1d', 'triangle': 'triangle_plot'}[plot_type]
-        getattr(g, plot_func)(self.roots, params=params, filled=True)
+        getattr(g, plot_func)([mcsamples], params=params, filled=True)
 
         for param in params:
             ax = g.get_axes_for_params(param)
-            g.add_x_marker(marker=self.sim_values[param], \
-                ax=ax, ls='-', color = 'red')
-
-        for param in params:
-
-            ax = g.get_axes_for_params(param)
-
-            prior_dict = self.priors[param]['prior']
-
-            if 'dist' in prior_dict.keys():
-
-                if prior_dict['dist'] == 'norm':
-                    center = prior_dict['loc'] 
-                    sigma = prior_dict['scale'] 
-                    g.add_x_bands(center, sigma, ax=ax, alpha1 = 0.25, alpha2 = 0.2)
-                else:
-                    raise NotImplementedError
-
-            elif 'min' in prior_dict.keys():
-                xmin = prior_dict['min'] 
-                xmax = prior_dict['max'] 
-                center = (xmin + xmax)/2.0
-                sigma = (xmax - xmin)/2.0
-                g.add_x_bands(center, sigma, ax=ax, alpha1 = 0.5, alpha2 = 0)
+            self.add_input_line_for_param(g, ax, param)
+            self.add_prior_bands_for_param(g, ax, param)
 
         g.export(plot_name)
         self.logger.info('Saved plot: {}'.format(plot_name))
+
+    def get_mcsamples(self, g, params):
+        samples = g.sampleAnalyser.samplesForRoot(self.roots[0])
+        print('Number of points in chain = {}'.format(samples.weights.size))
+
+        p = samples.getParams()
+        sample_values = [getattr(p, param) for param in params]
+
+        ranges = self.get_mcsamples_ranges()
+        ranges['w'] = [-1, None] #HACK
+
+        MCSamples = getdist.MCSamples(
+            samples = sample_values, \
+            weights = samples.weights,\
+            loglikes = samples.loglikes, \
+            names = params,\
+            settings = self.analysis_settings, \
+            ranges = ranges, 
+        )
+        return MCSamples
+
+    def add_input_line_for_param(self, g, ax, param):
+        g.add_x_marker(marker=self.sim_values[param], \
+                ax=ax, ls='-', color = 'red')
+
+    def add_prior_bands_for_param(self, g, ax, param):
+        prior_dict = self.priors[param]['prior']
+        prior = Prior(prior_dict)
+        center, sigma = prior.get_center_and_sigma()
+        if prior.is_uniform():
+            g.add_x_bands(center, sigma, ax=ax, alpha1 = 0.5, alpha2 = 0)
+        else:
+            g.add_x_bands(center, sigma, ax=ax, alpha1 = 0.25, alpha2 = 0.2)
 
     def get_bias_default_values(self):
         """Returns a dictionary with bias name and default values"""
@@ -138,6 +155,46 @@ class ChainPlotter():
         params_info = info['params']
         return params_info
 
+    def get_mcsamples_ranges(self):
+        """Return a dictionary for parameter ranges in the GetDist format
+        given priors loaded from the cobaya updated yaml file."""
+        ranges = {}
+        priors = copy.deepcopy(self.priors)
+        for paramname in priors.keys():
+            try: 
+                pmin = priors[paramname]['prior']['min']
+                pmax = priors[paramname]['prior']['max']
+                ranges[paramname] = [pmin, pmax]
+            except Exception as e:
+                pass
+        return ranges
+
+
+class Prior():
+
+    def __init__(self, prior_dict):
+        self.prior_dict = prior_dict
+
+    def get_center_and_sigma(self):
+        if self.is_uniform():
+            xmin = self.prior_dict['min'] 
+            xmax = self.prior_dict['max'] 
+            center = (xmin+xmax) / 2.0
+            sigma = (xmax-xmin) / 2.0
+        else:
+            if self.prior_dict['dist'] == 'norm':
+                center = self.prior_dict['loc'] 
+                sigma = self.prior_dict['scale'] 
+            else:
+                raise NotImplementedError
+        return center, sigma
+    
+    def is_uniform(self):
+        is_min_given = 'min' in self.prior_dict.keys()
+        is_max_given = 'max' in self.prior_dict.keys()
+        is_uniform = (is_min_given and is_max_given)
+        return is_uniform
+
 
 if __name__ == '__main__':
     """Example usage: python3 -m analysis.plot ./analysis/inputs/ps_base.yaml"""
@@ -148,3 +205,4 @@ if __name__ == '__main__':
 
     plotter = ChainPlotter(args)
     plotter.plot_1d()
+
