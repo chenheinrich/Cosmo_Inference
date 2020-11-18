15d5fece7bc7b00e2829c5576de35b5f554e2835
Chen Heinrich <chenhe@caltech.edu>
2020-11-17 16:30:58 -0800

    added completed backbone for bispectrum theory code

--- theory/data_vector/data_vector.py
+++ theory/data_vector/data_vector.py
@@ -5,10 +5,47 @@ from theory.data_vector.data_spec import DataSpec
 class DataVector():
 
     def __init__(self, cosmo_par, survey_par, data_spec):
-        pass
+        self._cosmo_par = cosmo_par
+        self._survey_par = survey_par
+        self._data_spec = data_spec
+
+        self._grs_ingredients = self._get_grs_ingredients()
 
     def calculate(self):
         pass
 
     def save(self, fn):
         pass
+
+    def _get_grs_ingredients(self):
+        grs_ing = GRSIngredients(self._cosmo_par, self._survey_par)
+        return grs_ing
+
+
+class P3D(DataVector):
+
+    def __init__(self, cosmo_par, survey_par, ps3d_spec):
+        # TODO check that ps3d_spec is instance of the right child class?
+        super().__init__(cosmo_par, survey_par, ps3d_spec)
+        
+    def calculate(self):
+        #TODO to be implemented: 
+        self._galaxy_ps = None
+
+    def get_galaxy_ps(self):
+        return self._galaxy_ps
+
+
+class B3D(DataVector):
+
+    def __init__(self, cosmo_par, survey_par, bs3d_spec):
+        # TODO check that bs3d_spec is instance of the right child class?
+       super().__init__(cosmo_par, survey_par, bs3d_spec)
+
+    def calculate(self):
+        #TODO to be implemented: 
+        # using various ingredients self._grs_ingredients to make galaxy_bis 
+        self._galaxy_bis = None
+
+    def get_galaxy_bis(self):
+        return self._galaxy_bis
--- theory/data_vector/grs_ingredients.py
+++ theory/data_vector/grs_ingredients.py
@@ -1,7 +1,37 @@
 
+from theory.cosmo.cosmo_product import CosmoProduct
 
-class GRSIngredients():
+#TODO do this last!
 
-    def __init__(self):
+class GRSIngredients(object):
+
+    def __init__(self, cosmo_par, survey_par):
+        self._cosmo_par = cosmo_par
+        self._survey_par = survey_par
+        self._cosmo_product = self._get_cosmo_product()
+
+    def _get_cosmo_product(self):
+        return CosmoProduct(self._cosmo_par)
+
+    #TODO might want to make cosmo_products getter part of GRSIngredients 
+    # getter too, so nobody knows about cosmo_products really.
+    def get_b1(self):
+        """
+        docstring
+        """
         pass
+
+    def get_F2(self):
+        """
+        docstring
+        """
+        pass
+
+    def _get_bnl(self):
+        pass
+
+    def _get_alpha(self):
+        pass
+
+
 
