5760650870337b7163ac3d01d795e2ea1e20377b
Chen Heinrich <chenhe@caltech.edu>
2020-12-17 19:24:53 -0800

    added preliminary bispectrum plotter

--- theory/get_bis.py
+++ theory/get_bis.py
@@ -9,8 +9,16 @@ from theory.params.survey_par import SurveyPar
 from theory.data_vector.data_spec import DataSpec
 from theory.data_vector.data_vector import DataVector, B3D
 from theory.utils import file_tools
+from theory.plotting.bis_plotter import BisPlotter
 
-def get_bis(info):
+def get_data_spec(info):
+    survey_par_file = info['survey_par_file']
+    data_spec_dict = info['Bispectrum3D'] 
+    survey_par = SurveyPar(survey_par_file)
+    data_spec = DataSpec(survey_par, data_spec_dict)
+    return data_spec
+
+def get_data_vec(info):
     
     cosmo_par_file = info['cosmo_par_file']
     cosmo_par_fid_file = info['cosmo_par_fid_file']
@@ -24,6 +32,11 @@ def get_bis(info):
     data_spec = DataSpec(survey_par, data_spec_dict)
 
     data_vec = B3D(cosmo_par, cosmo_par_fid, survey_par, data_spec)
+    
+    return data_vec
+
+def get_bis(info):
+    data_vec = get_data_vec(info)
     galaxy_bis = data_vec.get('galaxy_bis')
     print('galaxy_bis.shape = {}'.format(galaxy_bis.shape))
     return galaxy_bis
@@ -52,3 +65,7 @@ if __name__ == '__main__':
     bggg = get_bis(info)
     fn = get_fn(info)
     file_tools.save_file_npy(fn, bggg)
+
+    data_spec = get_data_spec(info)
+    bis_plotter = BisPlotter(bggg, data_spec)
+    bis_plotter.make_plots()
