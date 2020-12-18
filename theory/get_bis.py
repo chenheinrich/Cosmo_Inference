e774a3fb36f74e5bc78f51b5716bd222dad2cfd3
Chen Heinrich <chenhe@caltech.edu>
2020-12-17 18:48:47 -0800

    validated standalone power spectrum calculations

--- theory/get_bis.py
+++ theory/get_bis.py
@@ -7,25 +7,8 @@ import pickle
 from theory.params.cosmo_par import CosmoPar
 from theory.params.survey_par import SurveyPar
 from theory.data_vector.data_spec import DataSpec
-from theory.data_vector.data_vector import DataVector, P3D, B3D
+from theory.data_vector.data_vector import DataVector, B3D
 from theory.utils import file_tools
-
-def get_ps(info):
-    
-    cosmo_par_file = info['cosmo_par_file']
-    cosmo_par_fid_file = info['cosmo_par_fid_file']
-    survey_par_file = info['survey_par_file']
-    data_spec_dict = info['PowerSpectrum3D'] 
-
-    cosmo_par = CosmoPar(cosmo_par_file)
-    cosmo_par_fid = CosmoPar(cosmo_par_fid_file)
-
-    survey_par = SurveyPar(survey_par_file)
-    data_spec = DataSpec(survey_par, data_spec_dict)
-
-    data_vec = P3D(cosmo_par, cosmo_par_fid, survey_par, data_spec)
-    galaxy_ps = data_vec.get('galaxy_ps')
-    return galaxy_ps
 
 def get_bis(info):
     
@@ -48,19 +31,6 @@ def get_bis(info):
 def get_fn(info):
     file_tools.mkdir_p(info['plot_dir'])
     return os.path.join(info['plot_dir'], info['run_name'] + '.npy')
-
-#TODO to move to a different file
-def compare_galaxy_ps(d1, d2):
-    if np.allclose(d1, d2):
-        print('Passed!')
-    else:
-        diff = d2 - d1
-        frac_diff = diff/d1
-        max_diff = np.max(np.abs(diff))
-        max_frac_diff = np.max(np.abs(frac_diff))
-        print('max_diff', max_diff)
-        print('max_frac_diff', max_frac_diff)
-
 
 if __name__ == '__main__':
     """
