added code for generic derivative and fisher computation; need to fix derivative convergence and add proper invcov for bispectrum multipoles

galaxy_3d/theory/covariance/bis_mult_covariance.py
@@ -5,7 +5,7 @@ from theory.data_vector.data_vector import DataVector, Bispectrum3DRSD
 from theory.data_vector.data_spec import Bispectrum3DRSDSpec
 from theory.math_utils.spherical_harmonics import SphericalHarmonicsTable
 
-class BispectrumMultipole(DataVector):
+class BispectrumMultipoleCovariance(DataVector):
     """This class takes in the 3D Fourier galaxy bispectrum results  
     and performs the integral over the spherical harmonics to
     return the bispectrum multipoles"""
@@ -23,11 +23,7 @@ class BispectrumMultipole(DataVector):
         super().__init__(grs_ingredients, survey_par, bis_mult_spec)
 
         self._b3d_rsd = self._get_b3d_rsd(grs_ingredients, survey_par, bis_mult_spec)
-        self._ylms = self._get_ylms(spherical_harmonics_table)   
-
-    @property
-    def nlm(self):
-        return self._data_spec.nlm
+        self._ylms = self._get_ylms(spherical_harmonics_table)    
 
     def _setup_allowed_names(self):
         self._allowed_names = ['galaxy_bis']
@@ -81,7 +77,7 @@ class BispectrumMultipole(DataVector):
         # for this integral (same lm gives 1, not same lm gives 0)
         one = dOmega * np.matmul(np.transpose(self._ylms), np.conj(self._ylms))
         print('expect identity matrix: ', one)
-        passed_test = np.allclose(np.diag(np.ones(self.nlm)), one)
+        passed_test = np.allclose(np.diag(np.ones(9)), one)
         print('Orthogonality of ylms for given sampling in (theta, phi): test passed?', passed_test)
         # TODO need to test for convergence of final integral for bispectrum
         # might want to just interpolate bispectrum if it varies slower than ylms
@@ -89,7 +85,7 @@ class BispectrumMultipole(DataVector):
 
         galaxy_bis_mult = dOmega * np.matmul(galaxy_bis, np.conj(self._ylms)) 
         assert galaxy_bis_mult.shape[0:3] == galaxy_bis.shape[0:3]
-        assert galaxy_bis_mult.shape[-1] == self.nlm
+        assert galaxy_bis_mult.shape[-1] == self._data_spec.nlm
 
         return galaxy_bis_mult
 

galaxy_3d/theory/params/cosmo_par.py
@@ -2,11 +2,11 @@ import numpy as np
 import yaml
 
 class CosmoPar(object):
-    def __init__(self, cosmo_par_file):
+    def __init__(self, cosmo_par_file, overwrite_dict=None):
         self.cosmo_par_file = cosmo_par_file
-        self._set_par()
+        self._set_par(overwrite_dict)
 
-    def _set_par(self):
+    def _set_par(self, overwrite_dict):
 
         #TODO add error handling
         with open(self.cosmo_par_file) as f:
@@ -15,15 +15,21 @@ class CosmoPar(object):
         for k, v in info.items():
             setattr(self, k, v)
 
-        self._add_par()
+        if overwrite_dict is not None:
+            for k, v in overwrite_dict.items():
+                setattr(self, k, v)
+
+        self._add_aux_par()
         
         #TODO turn into unit test
         # print('self.fnl', self.fnl)
 
-    def _add_par(self):
+    def _add_aux_par(self):
 
         if (hasattr(self, 'logA') is True) and (hasattr(self, 'As') is False):
             self.As = 1e-10*np.exp(self.logA)
 
         if (hasattr(self, 'theta_MC_100') is True) and (hasattr(self, 'cosmomc_theta') is False):
             self.cosmomc_theta = self.theta_MC_100/100.0
+
+    