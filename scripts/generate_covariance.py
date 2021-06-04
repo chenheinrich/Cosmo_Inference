fixed factor of 2 bug in PS covariance calculation; updated tests to reflect that; rearranged covariance to be of shape (nb, nb, nz, nk, nmu).

scripts/generate_covariance.py
@@ -14,9 +14,12 @@ from spherelikes.utils.log import class_logger
 from spherelikes.params import SurveyPar
 
 from lss_theory.utils.profiler import profiler
+from lss_theory.math_utils.matrix import check_matrix_symmetric
 from scripts.invert_matrix import invert_block_matrix
 from copy import deepcopy
 
+#TODO Need to clean up old methods
+# old format of cov calculation can go now
 class CovCalculator():
 
     def __init__(self, data, args):
@@ -74,13 +77,12 @@ class CovCalculator():
         self.survey_par = SurveyPar(self.args['survey_par_file'])
         h = self.data['H0'] / 100.0 
         self.number_density = h**3 * self.survey_par.get_number_density_array()
-        # TODO test change in results with this
-        # TODO any tests need to be rewritten?
 
     def get_and_save_invcov(self):
         self.get_cov()
         self.get_invcov()
-        self.do_inv_test(self.cov, self.invcov)
+        #TODO need to 
+        #self.do_inv_test(self.cov, self.invcov)
         self.save()
 
     def get_cov(self):
@@ -95,27 +97,85 @@ class CovCalculator():
 
         self.get_data_1d()
         self.shape = self.galaxy_ps.shape
-        ntot = np.prod(self.shape)
 
-        self.cov = np.zeros((ntot, ntot))
-        for ips1 in range(self.nps):
-            for ips2 in range(self.nps):
-                print('(ips1, ips2) = ({} {})'.format(ips1, ips2))
-                block_size = np.prod(self.shape[1:])
-                ind1 = ips1 * block_size
-                ind2 = ips2 * block_size
-                block = self.get_block(ips1, ips2)
-                self.cov[ind1:(ind1 + block_size), ind2:(ind2 + block_size)]\
-                    = block
+        (self.nps, self.nz, self.nk, self.nmu) = self.shape
+        #ntot = np.prod(self.shape)
 
-        self.block_size = block_size
-        self.logger.info('self.shape = {}'.format(self.shape))
-        self.logger.info('self.block_size = {}'.format(self.block_size))
+        #self.cov = np.zeros((ntot, ntot))
+        self.cov = np.zeros((self.nps, self.nps, self.nz, self.nk, self.nmu))
+
+        for iz in range(self.nz):
+            for ik in range(self.nk):
+                for imu in range(self.nmu):
+                    #for ips1 in range(self.nps):
+                    #    for ips2 in range(self.nps):
+                    #        print('(ips1, ips2) = ({} {})'.format(ips1, ips2))
+                    #        block_size = np.prod(self.shape[1:])
+                    #        ind1 = ips1 * block_size
+                    #        ind2 = ips2 * block_size
+                    #        block = self.get_block(ips1, ips2)
+                    #        self.cov[ind1:(ind1 + block_size), ind2:(ind2 + block_size)]\
+                    #            = block
+
+                    self.cov[:, :, iz, ik, imu] = self.get_cov_smallest_nondiagonal_block(iz, ik, imu)
+
+
+        nmodes_z_k_mu = self.get_nmodes_z_k_mu(fsky=self.fsky)
+        self.cov = self.cov / (nmodes_z_k_mu[np.newaxis, np.newaxis, :, :, :])
 
         return self.cov
 
+    def get_cov_smallest_nondiagonal_block(self, iz, ik, imu):
+        cov = self.get_cov_nps_x_nps_block(iz, ik, imu)   
+        assert check_matrix_symmetric(cov)     
+        return cov
+
+    #@profiler
+    def get_cov_nps_x_nps_block(self, iz, ik, imu):
+
+        nps = self.nps
+        cov = np.zeros((nps, nps))
+
+        for ips in range(nps):
+            for jps in range(nps):
+
+                (a, b) = self.dict_sample_pair_from_ips['%i' % ips]
+                (c, d) = self.dict_sample_pair_from_ips['%i' % jps]
+
+                ips1 = self.dict_ips_from_sample_pair['%i,%i' % (a, c)]
+                ips2 = self.dict_ips_from_sample_pair['%i,%i' % (b, d)]
+                ips3 = self.dict_ips_from_sample_pair['%i,%i' % (a, d)]
+                ips4 = self.dict_ips_from_sample_pair['%i,%i' % (b, c)]
+
+                cov[ips, jps] = self.get_observed_ps_from_ips_iz_ik_imu(ips1, iz, ik, imu) \
+                        * self.get_observed_ps_from_ips_iz_ik_imu(ips2, iz, ik, imu) \
+                    + self.get_observed_ps_from_ips_iz_ik_imu(ips3, iz, ik, imu) \
+                        * self.get_observed_ps_from_ips_iz_ik_imu(ips4, iz, ik, imu)
+
+                # TODO not accounting for equilateral and isoceles triangles yet 
+                # TODO for these other terms in the cov, will need to change fog definition
+
+        return cov
+
     @profiler
     def get_invcov(self, rescale=1.0):
+
+        try:
+            self.invcov = np.zeros_like(self.cov)
+
+            for iz in range(self.nz):
+                for ik in range(self.nk):
+                    for imu in range(self.nmu):
+                        self.invcov[:,:,iz,ik,imu] = np.linalg.inv(self.cov[:,:,iz,ik,imu] * rescale)
+                        assert check_matrix_symmetric(self.invcov[:,:,iz,ik,imu])             
+            
+            return self.invcov
+
+        except Exception as e:
+            self.logger.error('Error: {}'.format(e))
+
+    @profiler
+    def get_invcov_old_format(self, rescale=1.0):
         try:
             self.invcov = np.linalg.inv(self.cov * rescale)
             self.logger.debug(
@@ -136,7 +196,7 @@ class CovCalculator():
             self.logger.error('Error: {}'.format(e))
 
     @staticmethod
-    def do_inv_test(mat, invmat, threshold=1e-3):
+    def do_inv_test_old_format(mat, invmat, threshold=1e-3):
         id0 = np.diag(np.ones(mat.shape[0]))
         id1 = np.matmul(mat, invmat)
         id2 = np.matmul(invmat, mat)
@@ -205,6 +265,11 @@ class CovCalculator():
         ps = self.get_galaxy_ps(a, b) + self.get_noise(a, b)
         return ps
 
+    def get_observed_ps_from_ips_iz_ik_imu(self, ips, iz, ik, imu):
+        (a, b) = self.dict_sample_pair_from_ips['%i' % ips]
+        ps = self.galaxy_ps[ips, iz, ik, imu] + self.get_noise(a, b)[iz, ik, imu] #TODO need to optimize
+        return ps
+
     def get_nmodes_z_k_mu(self, fsky=1.0):
         if not hasattr(self, 'nmodes_z_k_mu'):
             self.__make_nmodes()
@@ -231,8 +296,12 @@ class CovCalculator():
     def __make_nmodes(self):
         """Returns a 3d numpy array of shape (nz, nk, nmu) for Nmodes
         needed in a Cov[P_{j1, j2} P{j1', j2'}] block of the covariance,
-        where Cov = (2 / Nmodes) * (P + 1/n)^2, and
-        Nmodes = Vsurvey * (2 pi k^2 dk dmu)/ (2 pi)^3 with fsky = 1.
+        where 
+            Cov = (Nmodes)^{-1} * 
+                [(P{j1, j1'} + 1/n)(P{j2, j2'} + 1/n) 
+                + (P{j1, j2'} + 1/n)(P{j2, j1'} + 1/n)],
+        and
+            Nmodes = Vsurvey * (2 pi k^2 dk dmu)/ (2 pi)^3 with fsky = 1.
         """
         # (nk, nmu)
         nmodes_k_mu = (self.k**2 * self.dk)[:, np.newaxis] * \
@@ -279,7 +348,7 @@ class CovCalculator():
         block21 = self.cov[ind2:(ind2 + block_size), ind1:(ind1 + block_size)]
         assert np.allclose(block12, block21)
 
-    def test_get_noise(self):
+    def test_get_noise(self): #TODO this might need to fixed, 1/h --> 1/h^2 due to bug fix
 
         h = self.data['H0'] / 100.0
 

spherelikes/likelihood/LikePowerSpectrum3D.py
@@ -40,6 +40,35 @@ class LikePowerSpectrum3D(Likelihood):
             chi2 = 0.0
         else:
             delta = self.simulated_data - self.get_sampled_data()
+            
+            chi2 = 0.0
+            for iz in range(self.nz):
+                for ik in range(self.nk):
+                    for imu in range(self.nmu):
+
+                        tmp = np.matmul(self.invcov[:,:,iz,ik,imu], delta[:,iz,ik,imu])
+                        chi2 = chi2 + np.matmul(delta[:,iz,ik,imu], tmp)
+
+            self.logger.debug('chi2 = {}'.format(chi2))
+
+        return -0.5 * chi2
+
+    #@profiler
+    def logp_old_format(self, **params_values):
+        """
+        Taking a dictionary of (sampled) nuisance parameter values params_values
+        and return a log-likelihood.
+        """
+
+        # TODO Placeholder for future derived parameter and foreground calculations.
+        #derived_param = self.provider.get_param('derived_param')
+        my_foreground_amp = params_values['my_foreground_amp']
+
+        if self.is_reference_likelihood is True:
+            print('is_reference_likelihood = True == > return chi2=0.')
+            chi2 = 0.0
+        else:
+            delta = self.simulated_data - self.get_sampled_data()
 
             # TODO turn into official error handling
             print('delta.shape = {}'.format(delta.shape))
@@ -70,8 +99,8 @@ class LikePowerSpectrum3D(Likelihood):
 
         self.logger.info('Loading invcov ...')
 
-        n = np.prod(self.simulated_data.shape)
-        expected_shape = (n, n)
+        (self.nps, self.nz, self.nk, self.nmu) = self.simulated_data.shape
+        expected_shape = (self.nps, self.nps, self.nz, self.nk, self.nmu)
 
         try:
             self.logger.debug('self.invcov_path = {}'.format(self.invcov_path))